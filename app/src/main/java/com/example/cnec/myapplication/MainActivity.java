36e2ec15482f1fe976d5decb9da3bb811c5de186
evertondewes <everton.dewes@gmail.com>
2018-08-14 21:54:34 -0300

    adicionado as cores

--- app/src/main/java/com/example/cnec/myapplication/MainActivity.java
+++ app/src/main/java/com/example/cnec/myapplication/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.cnec.myapplication;
 
 import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,30 +12,57 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
+        TextView tv = (TextView) findViewById(R.id.tvNome);
+        tv.setText(tv.getText() + "\n" + "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        TextView tv = (TextView) findViewById(R.id.tvNome);
+        tv.setText(tv.getText() + "\n" + "onStart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        TextView tv = (TextView) findViewById(R.id.tvNome);
+        tv.setText(tv.getText() + "\n" + "onRestart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        TextView tv = (TextView) findViewById(R.id.tvNome);
+        tv.setText(tv.getText() + "\n" + "onPause");
+
+        ConstraintLayout cl = findViewById(R.id.telaInicial);
+        cl.setBackgroundColor(Color.BLACK);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        TextView tv = (TextView) findViewById(R.id.tvNome);
+        tv.setText(tv.getText() + "\n" + "onResume");
+
+        ConstraintLayout cl = findViewById(R.id.telaInicial);
+        cl.setBackgroundColor(Color.BLUE);
     }
 
     public void pressionarBotao(View v) {
-
-        EditText et = (EditText) findViewById(R.id.etNome);
-        EditText etS = (EditText) findViewById(R.id.etSobrenome);
         TextView tv = (TextView) findViewById(R.id.tvNome);
+        tv.setText(tv.getText() + "\n");
 
-        String nome = et.getText().toString();
-
-        String sobrenome = etS.getText().toString();
-
-
-        tv.setText(tv.getText() + "\n" +nome + " " + sobrenome);
-
-        Log.i("Nome", et.getText().toString());
+        ConstraintLayout cl = findViewById(R.id.telaInicial);
+        cl.setBackgroundColor(Color.YELLOW);
     }
 
-    public void pressionarBotao2222(View v) {
-
-        Log.v("teste1", "teste2");
-    }
 
 }
